c1f2236c7b8c61d56cd6735751fae4c48e3ac7ab
neels22 <indraneelsarode@gmail.com>
2025-02-06 14:22:01 -0800

    removed the string

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -7,7 +7,7 @@ const mongoose =require("mongoose")
 // the below gives a promise 
 const connectDB = async (  ) => {
     
-mongoose.connect("mongodb+srv://devtinder:lVQjZRGauAJhPZTo@neelcluster.9yali.mongodb.net/")
+mongoose.connect("")
 }
 
 module.exports= connectDB
